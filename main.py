9b02df7a17301b03b0db4623c13e83b00cb2ecb2
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-03-09 12:35:35 +0700

    feat: add `Python 2` check

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import sys
 
+if sys.version_info[0] < 3:
+    print("Please use Python 3 or above")
+    exit()
 
 try:
     import tabulate
     import alive_progress
 except ImportError:
-    print("Please install alive_progress using 'pip install alive-progress'")
-    print("Please install tabulate using 'pip install tabulate'")
+    print("Please install the dependencies using 'pip install -r requirements.txt'")
     exit()
 
 import src.patch
