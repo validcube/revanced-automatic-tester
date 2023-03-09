32928a91fd6fa49b9365d9c9346ba5943210a775
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-03-09 16:17:54 +0700

    refactor: cleanup

--- src/patch.py
+++ src/patch.py
@@ -5,7 +5,7 @@ from time import time
 # import subprocess
 import re
 
-from src.actionToTake import actionToTake as aTT
+from action_to_take import action_to_take as aTT
 
 try:
     from alive_progress import alive_bar
@@ -19,7 +19,7 @@ path = f"{os.getcwd()}/src/"
 
 rvcli = path + "revanced-cli-2.20.1-dev.1-all.jar"
 rvpatches = path + "revanced-patches-2.165.0-dev.9.jar"
-rvintegration = path+"revanced-integrations-0.100.0-dev.7.apk"
+rvintegration = path + "revanced-integrations-0.100.0-dev.7.apk"
 APK = "D:/HS-Storage/Project/revanced-automatic-tester/APK"
 input = "D:/HS-Storage/Project/revanced-automatic-tester/APK/Input"
 output = "D:/HS-Storage/Project/revanced-automatic-tester/APK/Output"
@@ -34,7 +34,7 @@ rvintegration_info = re.search(
     r"integrations-(\d+\.\d+\.\d+-\w+\.\d+)", rvintegration)
 
 experimental = True
-if experimental == True:
+if experimental is True:
     arguments = "--experimental"
 
 
@@ -50,48 +50,30 @@ def patch(input_directory):
 
                 cmd = f"java -jar {rvcli} -a \"{input}/{file}\" -o \"{output}/{file}\" -b \"{rvpatches}\" -m \"{rvintegration}\" {arguments} -c"
                 os.system(cmd)
+
                 print("\n")
 
-                # apk_name = file.split("_")[0]
-                # version = re.search(r"_([\d.]+)-\d+_minAPI", file).group(1)
                 actionToTake = aTT[1]
                 patching_status = "✅ Patching successful"
 
                 apk_info.append(
                     (apk_name, version, patching_status, actionToTake))
+                
                 print(f'Took {time()-ds} seconds to finish patching {file}')
-            except OSError or Exception as e:
-                print(e)
+            except OSError or Exception as error:
+                print(error)
                 print(
                     f'Error: Took {time()-ds} seconds to finish patching {file}')
-                patching_status = f"❎ Patching failed | {e}"
-                actionToTake = aTT[4]
+                patching_status = f"❎ Patching failed | {error}"
+                action_to_take = aTT[4]
                 apk_info.append(
-                    (apk_name, version, patching_status, actionToTake))
+                    (apk_name, version, patching_status, action_to_take))
 
     return apk_info
 
 
-def validate():
-    for file in os.listdir(input):
-        with alive_bar(1, title=f"Validating file {file}") as bar:
-            if file.endswith(".apk") or file.endswith(".keystore"):
-                bar()
-
-    print(f"✅ Operation done! Every file is valid!")
-
-
-def format_output(apk_info, style="github"):
-    # apk_info = []
-
-    # for file in os.listdir(output_directory):
-    #    if file.endswith(".apk"):
-    #        version = re.search(r"_([\d.]+)-\d+_minAPI", file).group(1)
-    #        apk_name = file.split("_")[0]
-    #        actionToTake = "✋ Require manual testing of the APK to ensure all patches are applied correctly"
-    #        patching_status = "✅ Patching successful"
-    #        apk_info.append((apk_name, version, patching_status, actionToTake))
-    print(tabulate.tabulate(apk_info, headers=[
+def format_output(apks, style="github"):
+    print(tabulate.tabulate(apks, headers=[
           "APK name", "Version", "Status", "Action"], tablefmt=style))
     print(tabulate.tabulate([["ReVanced CLI", rvcli_info.group(1)], ["ReVanced Patches", rvpatches_info.group(1)], [
           "ReVanced Integrations", rvintegration_info.group(1)]], headers=["Tool", "Version"], tablefmt="github"))
@@ -108,19 +90,3 @@ format_output(apk_info, "github")
 
 
 print(f'Took {time()-ts} seconds to finish patching all files & formatting output into a table')
-
-# run patching in parallel using 4 processes
-# if __name__ == '__main__':
-#    processes = [multiprocessing.Process(target=patch, args=(file,)) for file in os.listdir(input)]
-#    # start all processes
-#    for process in processes:
-#        process.start()
-#        sleep(2.5)
-#    # wait for all processes to complete
-#    print('Waiting for the process...')
-#    for process in processes:
-#        process.join()
-
-# TODO
-# - Add support for human readable format
-# - Add support for GitHub MarkDown table format
