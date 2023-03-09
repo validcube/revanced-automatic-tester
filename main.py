a58557e5ef0819997525331add6a0ff97300cdaa
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-03-09 16:07:00 +0700

    chore: remove unused imports

--- main.py
+++ main.py
@@ -3,12 +3,5 @@ import sys
 if sys.version_info[0] < 3:
     print("Please use Python 3 or above")
     exit()
-
-try:
-    import tabulate
-    import alive_progress
-except ImportError:
-    print("Please install the dependencies using 'pip install -r requirements.txt'")
-    exit()
-
+    
 import src.patch
